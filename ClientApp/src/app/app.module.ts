675ccaf87ae8e525831e9f95549a6992cf46d1ac
maharoz <mahruj66@gmail.com>
2019-07-04 22:47:53 +0600

    input form design done

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import  { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { EmployeeComponent } from './employees/employee/employee.component';
 import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+import { EmployeeService } from './shared/employee.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +16,10 @@ import { EmployeeListComponent } from './employees/employee-list/employee-list.c
     EmployeeListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ClientApp/src/app/shared/employee.service.ts
+++ ClientApp/src/app/shared/employee.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Employee } from './employee.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-
+ formData : Employee;
   constructor() { }
 }
